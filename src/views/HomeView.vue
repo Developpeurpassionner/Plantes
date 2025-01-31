83dabdf5595c33cead3663d989c767d08f059402
Yékini ibrahim <97056077+Developpeurpassionner@users.noreply.github.com>
2025-01-31 22:25:30 +0100

    deuxieme commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,15 @@
 </script>
 
 <template>
-    <nav>
-        <img src="" alt="">
-        <div>
-            
+    <nav class="flex justify-between px-24 border border-gray-200 py-3 align-middle">
+        <div class="flex cursor-pointer">
+            <img class="w-10 h-12" src="../images/plante.png" alt="">
+            <p class="pt-4 text-2xl text-lime-400">Plantes</p>
         </div>
+        <ul class="flex justify-between w-150 text-lg pt-6 cursor-pointer">
+            <li>A propos</li>
+             <li>C'est quoi la plante ?</li>
+              <li>Variétés de plantes</li>
+        </ul>
     </nav>
 </template>
